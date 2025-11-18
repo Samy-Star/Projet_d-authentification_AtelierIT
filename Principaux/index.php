dcd340d789d5ab2cf9fa091bd5e7c092147face7
Samy-Star <awanyagracia@gmail.com>
2025-11-18 19:55:55 +0000

    Modification de la page index

---
+++ Principaux/index.php
@@ -0,0 +1,36 @@
+<?php 
+//Démarrage de la session et vérification de la connectivité de l'utilisateur
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Agence de voyage - Accueil</title>
+        <link rel="stylesheet" href="others/design.css">
+    </head>
+
+    <body>
+        <?php include "others/navbar.php";?>
+
+        <main>
+            <section>
+                <div>
+                    <h1>Bienvenue chez Fly High</h1>
+                    <p>Découvrez le mode à votre rythme</p>
+                    <?php if (!isset($_SESSION['user_id'])):?>
+                        <a href="login.php" class="button">Se connecter</a>
+                        <a href="register.php" class="button">S'inscrire</a>
+                    <?php else :?> <!--Redirection vers la page d'accueil si l'utilisateur est connecté-->
+                        <a href="accueil.php" class="button">Accéder à mon compte</a>
+                    <?php endif;?>
+                </div>
+            </section>
+        </main>
+
+        <?php include "others/footer.php";?>
+
+        <script src="others/script.js"></script>
+    </body>
+</html>
