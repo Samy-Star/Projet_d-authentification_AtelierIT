06ad718f94df9827975157963b9e5c1cee5898bd
Samy-Star <awanyagracia@gmail.com>
2025-11-18 21:55:28 +0000

    Mise en place des paries de la page

---
+++ others/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="footer-content">
+        <p><?php echo date("Y");?> Fly High. Tous droits réservés</p>
+        <p>
+            <a href="#">Politique de confidentialité</a>
+        </p>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ others/header.php
@@ -0,0 +1,21 @@
+<?php
+//Démarrage d'une session si aucune en cours
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Fly High</title>
+
+        <!--Fichier CSS-->
+        <link rel="stylesheet" href="design.css">
+        <!--Fichier JS-->
+        <script src="script.js" defer></script>
+    </head>
+
+    <body></body>
+</html>
