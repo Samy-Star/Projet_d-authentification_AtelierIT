38a7d21af3c56b910df7d6e4e6cdd7b9ca8c9e7d
Samy-Star <awanyagracia@gmail.com>
2025-12-22 20:39:19 +0000

    Remove password from DB connection string

--- Configuration/DB.php
+++ Configuration/DB.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=Auth_DB", "root", "@Always_and_Forever160122");
+    $pdo = new PDO("mysql:host=localhost;dbname=Auth_DB", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Erreur" . $e->getMessage();
